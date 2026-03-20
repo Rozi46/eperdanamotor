6de8042e35631b738630d4612c0f69ebea1bb73e
systemR <roziyunaldo55@gmail.com>
2026-03-20 10:39:31 +0700

    update fitur perdana motor

--- resources/views/admin/AdminOne/home.blade.php
+++ resources/views/admin/AdminOne/home.blade.php
@@ -7,7 +7,7 @@
                 <div class="container-fluid text-left">
                     <div class="row">
                         <div class="col-md-12 bg_con_dash">
-                            <div class="col-md-12 hd_page_main">Dashboard</div>
+                            <div class="col-md-12 hd_page_main">DashboardRR</div>
                             <!-- <div class="col-md-12 _con_dash"> -->
                                 <!-- <div class="row bg_con_dash">  -->
                                     <!-- @if($level_user['historypembelianbarang'] == 'Yes')
